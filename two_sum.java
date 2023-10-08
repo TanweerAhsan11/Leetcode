215b52e130faabf0dcfbf614bea022f2613d02ac
Tanweer Ahsan <104005918+TanweerAhsan11@users.noreply.github.com>
2023-10-08 23:40:33 +0530

    Update two_sum.java

--- two_sum.java
+++ two_sum.java
@@ -2,14 +2,14 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++)
         {
-            for(int j=i+1;j<nums.length;j++)
+            for(int j=i+1;j<nums.length;j++)        // we are taking j=i+1, since we dont need to check from 0th index. 
             {
-                if(nums[j] + nums[i] == target)
+                if(nums[j] + nums[i] == target)    
                 {
-                    return new int[]{i,j};
+                    return new int[]{i,j};        // if sum of array == target we return index of i and j.
                 }
             }
         }
-        return new int[]{};
+        return new int[]{};                      // we return nothing if sum of array is != target. 
     }
 }
